5cb392cd99b03ebdfca594a24066fe396ba5fbc5
train <train@TRN4865.dmztrn.amdocs.trn>
2019-07-24 14:58:54 +0530

    added everything

---
+++ src/main/java/org/amdocs/Test.java
@@ -0,0 +1,28 @@
+package org.amdocs;
+
+
+
+public class Test {
+
+	public double addition(double one, double two) {
+		
+		return one+two;
+	}
+
+	public double subtraction(double one, double two) {
+		
+		return one-two;
+	}
+
+	public double multiplication(double one, double two) {
+		
+		return one*two;
+	}
+
+	public double division(double one, double two) {
+		
+		return one/two;
+	}
+
+}
+
